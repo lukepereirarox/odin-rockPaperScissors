5779e3c27a84c57342a467f500fed661efd36d7d
Luke Pereira <142576471+lukepereirarox@users.noreply.github.com>
2024-06-25 18:00:15 +0530

    added basic playRound functionality

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const choices = ["rock", "paper", "scissors"];
 const typeChart = {
-    rock: {weakTo: 'paper', strongTo: 'scissors'},
-    paper: {weakTo: 'scissors', strongTo: 'rock'},
-    scissors: {weakTo: 'rock', strongTo: 'paper'}
- }
-
+  rock: { weakTo: "paper", strongTo: "scissors" },
+  paper: { weakTo: "scissors", strongTo: "rock" },
+  scissors: { weakTo: "rock", strongTo: "paper" },
+};
+let humanScore = 0;
+let compScore = 0;
 function getComputerChoice() {
   let i = Math.floor(Math.random() * 3);
   let compChoice = choices[i];
@@ -31,6 +32,17 @@ if (!choices.includes(getHumanChoice)) {
   getHumanChoice();
 }
 
-function playRound(humanChoice, compChoice){
+function playRound(humanChoice, compChoice) {
+  if (humanChoice === compChoice) {
+    console.log("Tie");
+  } else if (typeChart(humanChoice).strongTo === compChoice) {
+    console.log("You win!");
+    humanScore++;
+  } else if (typeChart(humanChoice).weakTo === compChoice) {
+    console.log("You Lose!");
+    compScore++;
+  }
 
+  console.log(humanScore);
+  console.log(compScore);
 }
