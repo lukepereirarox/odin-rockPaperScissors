cf295223a6c0ea1909bd12dca3f55728d115eace
Luke Pereira <142576471+lukepereirarox@users.noreply.github.com>
2024-06-26 12:00:53 +0530

    Added playGame function Moved playRound into playGame added final Score checker

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ const typeChart = {
   paper: { weakTo: "scissors", strongTo: "rock" },
   scissors: { weakTo: "rock", strongTo: "paper" },
 };
-let humanScore = 0;
-let compScore = 0;
+
 function getComputerChoice() {
   let i = Math.floor(Math.random() * 3);
   let compRan = choices[i];
-  console.log("Computer choice:" + compRan);
+  console.log("Computer choice: " + compRan);
   return compRan;
 }
 
@@ -26,41 +25,60 @@ function getHumanChoice() {
   return userChoice;
 }
 
-function playRound(h, c) {
-  if (h === c) {
-    console.log("Tie");
-    alert("Tie");
-  } else if (typeChart[h].strongTo === c) {
-    console.log("You win!");
-    alert("You win!");
-    humanScore++;
-  } else if (typeChart[h].weakTo === c) {
-    console.log("You Lose!");
-    alert("You lose!");
-    compScore++;
-  }
-
-  console.log(humanScore);
-  console.log(compScore);
-}
-
-let play = "y";
-
-while (play === "y") {
-  let humanChoice = getHumanChoice();
-  while (1) {
-    if (!choices.includes(humanChoice)) {
-      alert("Enter a Valid Option");
-      humanChoice = getHumanChoice();
-    } else {
-      console.log("Human Choice:" + humanChoice);
-      break;
+function playGame() {
+  function playRound(h, c) {
+    if (h === c) {
+      console.log("Tie");
+      alert("Tie");
+    } else if (typeChart[h].strongTo === c) {
+      console.log("You win!");
+      alert("You win!");
+      humanScore++;
+    } else if (typeChart[h].weakTo === c) {
+      console.log("You Lose!");
+      alert("You lose!");
+      compScore++;
     }
+
+    console.log(humanScore);
+    console.log(compScore);
   }
 
-  let compChoice = getComputerChoice();
-  playRound(humanChoice, compChoice);
-  play = window.prompt("Play again?(y/n) ");
+  let humanScore = 0;
+  let compScore = 0;
+  for (let i = 0; i < 5; i++) {
+    let humanChoice = getHumanChoice();
+    while (1) {
+      if (!choices.includes(humanChoice)) {
+        alert("Enter a Valid Option");
+        humanChoice = getHumanChoice();
+      } else {
+        console.log("Human Choice: " + humanChoice);
+        break;
+      }
+    }
+
+    let compChoice = getComputerChoice();
+    playRound(humanChoice, compChoice);
+  }
+
+  if (humanScore === compScore) {
+    alert("It's a Tie");
+    console.log("Final Tie");
+  } else if (humanScore < compScore) {
+    alert(
+      "The Computer Wins!\nYour Score: " +
+        humanScore +
+        "\nComputer Score : " +
+        CompScore
+    );
+    console.log("Final Loss");
+  } else {
+    alert(
+      "You win!\nYour Score: " + humanScore + "\nComputer Score: " + compScore
+    );
+    console.log("Final Win");
+  }
 }
 
-alert("End of the Day's play!");
+playGame();
